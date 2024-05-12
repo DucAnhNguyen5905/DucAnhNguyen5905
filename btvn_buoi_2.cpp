09f2a8c41cfa0101f4475afa43715b155d43b169
Duc Anh <duc-anh@gmail.com>
2024-05-12 13:06:31 +0700

    khoi tao 2

---
+++ btvn_buoi_2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std; 
+/*
+// Bai 11
+int main() {
+    int so_cac_chu_so;
+    cout << "Co bao nhieu so trong day so can tinh ?: " << endl;
+    cin >> so_cac_chu_so;
+
+    int sum = 0;
+    for (int i = 0; i < so_cac_chu_so; ++i) {
+        int cac_so_trong_day;
+        cout << "Nhap so thu " << i + 1 << ": ";
+        cin >> cac_so_trong_day;
+        sum += cac_so_trong_day; 
+    }
+
+    cout << "Tong cua day so: " << sum << endl;
+
+    return 0;
+}
+*/
+
+/*
+// Bai 10
+int main() {
+	int day_so[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	string chu_so[10] = {"mot", "hai", "ba", "bon", "nam", "sau", "bay", "tam", "chin", "muoi"};
+	
+	for (int i = 0; i < 10; i++) {
+		cout << " So " << day_so[i] << " duoc viet la " << chu_so[i] << endl;}
+	return 0;
+	}
+*/	
+
+// Bai 8
+
+					
+			
+        
+
+
+
+
+
+
+
+
+
